75c078f7ddec39e6af315b013af5ef5cbb1e5920
lalala <oyvinrog@gmail.com>
2025-03-22 23:57:16 +0100

    updaate version

--- sqlshell/__init__.py
+++ sqlshell/__init__.py
@@ -2,7 +2,7 @@
 SQLShell - A powerful SQL shell with GUI interface for data analysis
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __author__ = "SQLShell Team"
 
 # SQLShell package initialization 
